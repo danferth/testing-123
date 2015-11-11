879d5bd4e13a994655ae65b21228421bf79a1a54
Dan Klotz <danferth@gmail.com>
2015-11-11 17:15:24 +0000

    changed over to php base

---
+++ parts/foot.php
@@ -0,0 +1,5 @@
+<!--Javascript-->
+<script type="text/javascript" src="https://cdn.jsdelivr.net/g/jquery@2.1.4,angularjs@1.4.7,foundation@5.5.3,jquery.waypoints@2.0.5(waypoints.min.js+shortcuts/infinite-scroll/waypoints-infinite.min.js+shortcuts/sticky-elements/waypoints-sticky.min.js),sweetalert@1.1.3,gsap@12(TweenMax.min.js)"></script>
+<script type="text/javascript" src="assets/js/app.js"></script>
+</body>
+</html>
---
+++ parts/head.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en" ng-app="test">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<title>..::|TEST|::..</title>
+<meta name="description" content="...">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,200,700' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/g/foundation@5.5.3(css/foundation.min.css),sweetalert@1.1.3(sweetalert.css)" type="text/css" />
+<link rel="stylesheet" href="assets/css/style.css" type="text/css" />
+
+</head>
+<body>
